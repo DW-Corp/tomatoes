abf25502f287f4e75327257ee1187688047d36be
Danrley Pereira <danrleywillian@gmail.com>
2024-04-21 06:04:25 -0300

    release: production

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: '587',
+  address: 'smtpout.secureserver.net',
+  port: '465',
   authentication: :plain,
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  domain: 'heroku.com'
+  domain: 'dwcorp.com.br'
 }
 ActionMailer::Base.delivery_method = :smtp
